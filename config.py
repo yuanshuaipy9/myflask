ec6db674d3e6e6976af21096d67d8dedbfae66ec
ys <1025977912@qq.com>
2019-06-21 15:30:06 +0800

    集成日志到项目

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import logging
+
 from redis import StrictRedis
 
 
@@ -25,6 +27,11 @@ class Config(object):
     # 设置过期时间
     PERMANENT_SESSION_LIFETIME = 86400 * 2
 
+    # 设置日志等级
+    LOG_LEVEL = logging.DEBUG
+
+
+
 class DevelopmentConfig(Config):
     """开发环境下的配置"""
     DEBUG = True
@@ -32,6 +39,7 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     """生产环境下的配置"""
     DEBUG = True
+    LOG_LEVEL = logging.WARNING
 
 class TestingConfig(Config):
     """单元测试环境下的配置"""
--- info/__init__.py
+++ info/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_session import Session
@@ -10,7 +13,21 @@ from config import config
 # 在Flask很多扩展里面都可以先初始化扩展的对象，然后再去调用init_app方法去初始化
 db=SQLAlchemy()
 
+def setup_log(config_name):
+    # 设置日志的记录等级
+    logging.basicConfig(level=config[config_name].LOG_LEVEL)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
+
+
 def create_app(config_name):
+    setup_log(config_name)
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
