30f5d8da839d61a6a801a07a78e255cf983ede48
ys <1025977912@qq.com>
2019-06-23 15:00:55 +0800

    登陆的前端逻辑实现

--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -97,19 +97,42 @@ $(function(){
     $(".login_form_con").submit(function (e) {
         e.preventDefault()
         var mobile = $(".login_form #mobile").val()
-        var password = $(".login_form #password").val()
+        var passport = $(".login_form #password").val()
 
         if (!mobile) {
             $("#login-mobile-err").show();
             return;
         }
 
-        if (!password) {
+        if (!passport) {
             $("#login-password-err").show();
             return;
         }
 
         // 发起登录请求
+        var params = {
+            "mobile": mobile,
+            "passport": passport
+        }
+        $.ajax({
+            url: "/passport/login",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 代表登录成功
+                    location.reload()
+                }else {
+                    alert(resp.errmsg)
+                    $("#login-password-err").html(resp.errmsg)
+                    $("#login-password-err").show()
+                }
+            }
+        })
+
+
+
     })
 
 
