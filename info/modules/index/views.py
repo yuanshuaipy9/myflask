fb52a8c5b9bbbd0c2a0cee3f639574f3b85e1d56
ys <1025977912@qq.com>
2019-06-25 10:39:57 +0800

    首页新闻列表数据的前端实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,48 @@
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 
 from info.models import User, News
+from info.utils.response_code import RET
 from . import index_blu
+
+@index_blu.route("/news_list")
+def news_list():
+    cid=request.args.get("cid",1)
+    page = request.args.get("page", "1")
+    per_page = request.args.get("per_page", "10")
+
+    try:
+        page = int(page)
+        cid = int(cid)
+        per_page = int(per_page)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    filters=[]
+    if cid!=1:
+        filters.append(News.category_id==cid)
+
+    try:
+        paginate=News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page,per_page,False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    news_model_list=paginate.items
+    total_page=paginate.pages
+    current_page=paginate.page
+
+    news_dict_li=[]
+    for news in news_model_list:
+        news_dict_li.append(news.to_basic_dict())
+
+    data={
+       "total_page": total_page,
+        "current_page": current_page,
+        "news_dict_li": news_dict_li
+    }
+    return jsonify(errno=RET.OK, errmsg="OK",data=data)
 
 @index_blu.route("/")
 def index():
