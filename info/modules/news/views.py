ce118811f6b1be88b2578eb4fbcdc179523bfcf3
ys <1025977912@qq.com>
2019-06-25 17:50:46 +0800

    新闻详情页数据展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, current_app, session, g
+from flask import render_template, current_app, session, g, abort
 
 from info.models import User, News
 from info.utils.common import user_login_data
@@ -20,9 +20,21 @@ def news_detail(news_id):
     news_dict_li = []
     for news in news_list:
         news_dict_li.append(news.to_basic_dict())
+    news=[]
+    try:
+        news=News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        # 报404错误，404错误统一显示页面后续再处理
+        abort(404)
+    # 更新新闻的点击次数
+    news.clicks+=1
 
     data={
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
+        "news":news
     }
     return render_template("news/detail.html",data=data)
