6566b81c37e3bad1bebcd4f5e20c2f84d380ba4e
ys <1025977912@qq.com>
2019-06-26 09:15:16 +0800

    收藏和取消收藏后端逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,9 +1,60 @@
-from flask import render_template, current_app, session, g, abort
+from flask import render_template, current_app, session, g, abort, request, jsonify
 
 from info.models import User, News
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 from . import news_blu
 
+@news_blu.route("news_collect",methods=["post"])
+@user_login_data
+def collect_news():
+    """
+    收藏新闻
+    1.接收参数
+    2.判断参数
+    :return:
+    """
+    user=g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    news_id=request.json.get("news_id")
+    action = request.json.get("action")
+
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id=int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 查询新闻，并判断新闻是否存在
+    news=None
+    try:
+        news=News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    # 4.收藏以及取消收藏
+    if action=="cancel_collect":
+        # 取消收藏
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    else:
+        # 收藏
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
 
 @news_blu.route("/<int:news_id>")
 @user_login_data
