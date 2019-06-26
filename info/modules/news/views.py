e182897290cd397d24b66486ad7c131419456e1d
ys <1025977912@qq.com>
2019-06-26 16:26:11 +0800

    评论点赞和取消点赞的前后端逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,64 @@
 from flask import render_template, current_app, session, g, abort, request, jsonify
 
 from info import db
-from info.models import User, News, Comment
+from info.models import User, News, Comment, CommentLike
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
 from . import news_blu
+
+@news_blu.route("/comment_like",methods=["POST"])
+@user_login_data
+def comment_like():
+    user=g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    comment_id=request.json.get("comment_id")
+    action=request.json.get("action")
+
+    if not all([comment_id,action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["add","remove"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        comment_id=int(comment_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        comment=Comment.query.get(comment_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not comment:
+        return jsonify(errno=RET.NODATA, errmsg="评论不存在")
+
+    if action=="add":
+        comment_like_model =CommentLike.query.filter(CommentLike.comment_id==comment_id,CommentLike.user_id==g.user.id).first()
+        if not comment_like_model:
+            comment_like_model=CommentLike()
+            comment_like_model.user_id=user.id
+            comment_like_model.comment_id=comment.id
+            db.session.add(comment_like_model)
+            comment.like_count += 1
+    else:
+        comment_like_model = CommentLike.query.filter(CommentLike.user_id == user.id,CommentLike.comment_id == comment.id).first()
+        if comment_like_model:
+            db.session.delete(comment_like_model)
+            comment.like_count -= 1
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库操作失败")
+
+    return jsonify(errno=RET.OK, errmsg="OK")
+
 
 @news_blu.route("/news_comment",methods=["POST"])
 @user_login_data
