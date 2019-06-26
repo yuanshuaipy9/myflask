dde104b985d6d1077c3a48b9781a4889a9ee09c5
ys <1025977912@qq.com>
2019-06-26 10:47:36 +0800

    评论新闻和回复别人评论的前端逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -49,7 +49,7 @@ def comment_news():
         current_app.logger.error(e)
         db.session.rollback()
 
-    return jsonify(errmo=RET.OK,errmsg="OK",data=comment.to_dict())
+    return jsonify(errno=RET.OK,errmsg="OK",data=comment.to_dict())
 
 @news_blu.route("/news_collect",methods=["post"])
 @user_login_data
