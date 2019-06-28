5bdf5bdbe6d66e888ff6fa612f8226bf6cbffb17
ys <1025977912@qq.com>
2019-06-28 14:02:17 +0800

    上传图片的前后端代码实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,9 @@
-from flask import render_template, g, redirect, request, jsonify
+from flask import render_template, g, redirect, request, jsonify, current_app
 
+from info import constants
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.image_storage import storage
 from info.utils.response_code import RET
 
 
@@ -11,6 +13,27 @@ def pic_info():
     user=g.user
     if request.method=="GET":
         return render_template("news/user_pic_info.html",data={"user":user.to_dict()})
+
+    #  如果是POST表示修改头像
+    # 1. 取到上传的图片
+    try:
+        avatar=request.files.get("avatar").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 上传头像
+    try:
+        # 使用自已封装的storage方法去进行图片上传
+        key=storage(avatar)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传头像失败")
+
+    user.avatar_url=key
+    return jsonify(errno=RET.OK,errmsg="OK",data={"avatar_url":constants.QINIU_DOMIN_PREFIX+key})
+
+
 
 
 @profile_blu.route("/base_info",methods=["post","get"])
