c7c191c1cb75a8f7c1a8ae9f2c085e25cb8aa431
ys <1025977912@qq.com>
2019-06-28 12:29:43 +0800

    基本资料界面展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,13 @@
-from flask import render_template, g, redirect
+from flask import render_template, g, redirect, request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+
+@profile_blu.route("/base_info",methods=["post","get"])
+@user_login_data   # 用户必须登录才能进来
+def base_info():
+    if request.method=="GET":
+        return render_template("news/user_base_info.html",data={"user":g.user.to_dict()})
 
 
 @profile_blu.route("/info")
