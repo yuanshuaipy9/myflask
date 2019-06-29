290a7df826e3a17b0b80c27e38d324886b1d6002
ys <1025977912@qq.com>
2019-06-29 13:26:48 +0800

    集成创建管理员的函数到命令行

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask_migrate import Migrate, MigrateCommand
 from info import models
 from flask_script import Manager
 from info import create_app,db
+from info.models import User
 
 app=create_app("development")
 
@@ -12,6 +13,23 @@ Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.option("-n","-name",dest="name")
+@manager.option("-p","-password",dest="password")
+def createsuperuser(name,password):
+    if not all([name,password]):
+        print("参数不足")
+        user=User()
+        user.nick_name = name
+        user.mobile = name
+        user.password = password
+        user.is_admin = True
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+    print("添加成功")
 
 
 
